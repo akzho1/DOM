b5c73541006f258b9049deb7f1a89b27457a66eb
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-06-28 20:21:37 +0600

    phase

--- app.js
+++ app.js
@@ -6,17 +6,27 @@
 // button.addEventListener('click', handleClick)
 // buttonTwo.addEventListener('click', handleClick)
 // buttonTwo.removeEventListener('click' , handleClick)
-
+// const buttons = document.querySelectorAll('.btn');
+// // console.log(buttons);
+// // buttons.addEventListener('click', function() {
+// //   console.log('click')
+// // }) // Выходит ошибка
+// buttons.forEach(button =>{
+//     button.addEventListener('click', function() {
+//       console.log('clicked!') 
+//     })
+// })
 const buttons = document.querySelectorAll('.btn');
-
-// console.log(buttons);
-// buttons.addEventListener('click', function() {
-//   console.log('click')
-// }) // Выходит ошибка
-
-buttons.forEach(button =>{
-    button.addEventListener('click', function() {
-      console.log('clicked!')
-   
-    })
+const handleClick = (event) => {
+  console.log("target >",event.target)
+  console.log("curtarget >",event.currentTarget)
+  console.log(event.target === event.currentTarget)
+  // event.stopPropagation();
+}
+window.addEventListener('click', function(event) {
+  console.log("window click!", event.target)
+  event.stopPropagation();
+}, {capture:true})
+buttons.forEach(button => {
+  button.addEventListener('click', handleClick);
 })
