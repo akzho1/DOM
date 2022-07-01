e47c6b7a5b989de9e424af96f4f6334001d5756c
AKZHOL <46451454+akzho1@users.noreply.github.com>
2022-07-01 20:43:23 +0600

    dom

--- app.js
+++ app.js
@@ -54,26 +54,126 @@
 // const internalFn = external();
 // internalFn();
 
-function createAddress(type) {
-  const address = type.toUpperCase(); 
-  return function(name) {
-    return `${address} ${name}`;
-  }
+// function createAddress(type) {
+//   const address = type.toUpperCase(); 
+//   return function(name) {
+//     return `${address} ${name}`;
+//   }
+// }
+// const addressGrazhdanin = createAddress('Гражданин');
+// const addressGrazhdanka = createAddress('Гражданка');
+// console.log(addressGrazhdanin('Василий'))
+// console.log(addressGrazhdanka('Александра'))
+
+// function createPlayer(name) {
+//   let score = 0;
+//   return function scoreCount(){
+//     score++;
+//     return `${name} - ${score} баллов!`
+//   }
+// }
+
+// const playerOne = createPlayer('Василий')
+// const playerTwo = createPlayer('Александра')
+
+
+// Создать DIV
+const div = document.createElement('div');
+// Добавить к нему класс wrapper
+div.classList.add('wrapper');
+// Поместить его внутрь тега body
+const body = document.body; //document.querySelector('body');
+body.appendChild(div);
+// Создать заголовок H1 "DOM (Document Object Model)"
+const header = document.createElement('h1');
+header.textContent = 'DOM (Document Object Model)';
+// Добавить H1 перед DIV с классом wrapper
+div.insertAdjacentElement('beforebegin', header);
+// Создать список <ul> </ul>
+// Добавить в него 3 элемента с текстом "Один, два, три"
+const ul = `
+<ul>
+      <li>Один</li>
+      <li>Два</li>
+      <li>Три</li>
+</ul>
+`;
+// Поместить список внутрь элемента с классом wrapper
+div.innerHTML = ul;
+// Создать изображение
+const img = document.createElement('img');
+// Добавить атрбут source
+img.src = 'https://picsum.photos/240';
+// Добавить атрибут width со значением 240
+img.width = 240;
+// Добавить класс super
+img.classList.add('super');
+// Добавить свойство alt со значением "Super Man"
+img.alt = 'Super Man';
+// Поместить изображение внутрь элемента с классом wrapper
+div.appendChild(img);
+// Используя HTML строку, создать DIV с классом 'pDiv' + с 2-мя параграфами
+const elemHTML = `
+<div class='pDiv'>
+          <p>Параграф 1</p>
+          <p>Параграф 2</p>
+</div>`
+// Поместить этот DIV до элемента <ul></ul>
+const ulList = div.querySelector('ul');
+ulList.insertAdjacentHTML('beforebegin', elemHTML)
+// Добавить для 2-го параграфа класс text
+const pDiv = document.querySelector('.pDiv');
+pDiv.children[1].classList.add('text');
+// Удалить 1-й параграф
+pDiv.firstElementChild.remove();
+// Создать функцию generateAutoCard,
+// которая принимает 3 аргумента: brand, color, year
+const generateAutoCard = (brand, color, year) => {
+    const curDate = new Date();
+    const curYear = curDate.getFullYear();
+    return `
+            <div class="autoCard">
+            <div>
+                    <h2>${brand.toUpperCase()} ${year}</h2>
+                    <p>Автомобиль ${brand.toUpperCase()} - ${year} года. Возраст авто - ${curYear - year} лет.</p>
+                    <p>Цвет: ${color}</p>
+                    <button type='button' class='btn'>Удалить</button>
+                    </div>
+           </div>
+    `
 }
-const addressGrazhdanin = createAddress('Гражданин');
-const addressGrazhdanka = createAddress('Гражданка');
-console.log(addressGrazhdanin('Василий'))
-console.log(addressGrazhdanka('Александра'))
-
-function createPlayer(name) {
-  let score = 0;
-  return function scoreCount(){
-    score++;
-    return `${name} - ${score} баллов!`
-  }
+// Функция должна возрващать разметку HTML:
+// <div class="autoCard">
+// <h2> BRAND YEAR</h2>
+// <p>Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
+// </div>
+// Создать новый DIV с классом autos
+const carsDiv = document.createElement('div');
+carsDiv.classList.add('autos');
+// Создать 3 карточки авто, используя функцию generateAutoCard
+const carList = [
+  {brand: 'Tesla', year: 2015, color:'красный'},
+  {brand: 'Lexus', year: 2016, color:'серебристый'},
+  {brand: 'Nissan', year: 2012, color:'черный'}
+]
+const carsHTML = carList.map(car => {
+  return generateAutoCard(car.brand, car.color, car.year);
+}).join('');
+// Поместить эти 3 карточки внутрь DIV с классом autos
+carsDiv.innerHTML = carsHTML;
+// Поместить DIV с классом autos на страницу DOM - до DIV с классом wrapper
+div.insertAdjacentElement('beforebegin', carsDiv);
+// Добавить кнопку Удалить на каждую карточку авто
+// При клике на кнопку - удалять карточку из структуры DOM
+// 1. Выбрать все кнопки
+const buttons = document.querySelectorAll('.btn');
+// 2. Создать функцию удаления
+function handleClick(e) {
+  const currentButton = e.currentTarget;
+  currentButton.closest('.autoCard').remove();
+  // console.log(currentButton.parentElement)
 }
-
-const playerOne = createPlayer('Василий')
-const playerTwo = createPlayer('Александра')
-
-
+// 3. Использовать цикл - чтобы повестить обработчик события на каждую кнопку
+buttons.forEach(button =>{
+  button.addEventListener('click', handleClick)
+})
